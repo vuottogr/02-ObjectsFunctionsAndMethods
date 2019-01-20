79692b6881da4afb3f8eeda02b2ad675a1221ea9
vuottogr <vuottogr@rose-hulman.edu>
2019-01-19 21:24:18 -0500

    .

--- src/m2_functions.py
+++ src/m2_functions.py
@@ -3,11 +3,11 @@ Practice DEFINING and CALLING
      FUNCTIONS
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
-         Aaron Wilkin, their colleagues, and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         Aaron Wilkin, their colleagues, and Elle Vuotto.
+"""  # done: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 ###############################################################################
-# TODO: 2.
+# done: 2.
 #   Allow this module to use the  rosegraphics.py  module by marking the
 #     src
 #   folder in this project as a "Sources Root", as follows:
@@ -41,6 +41,7 @@ def main():
 #
 ###############################################################################
 
+a = sqrt(x ** (2) + y ** (2))
 
 ###############################################################################
 # TODO: 4a.  Define a function immediately below this _TODO_.
